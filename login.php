efe014db4fcbf9761c007e22353b1ebbd889eaca
Luciekimotho <luciekimotho@gmail.com>
2015-09-07 09:41:26 +0300

    personal website

---
+++ login.php
@@ -0,0 +1,17 @@
+<html>
+<a href = "elle.html">Home|</a>
+<a href = "about.html">About|</a>
+<a href = "blog.html">My blog|</a>
+<a href = "tech.html">Technology|</a>
+<a href = "photos.html">Photos</a>
+
+
+
+<body>
+<form method="post" action="http://localhost/Elle/welcome.html"> 
+ Username <input type = "text" name="username" /> </br>
+ Email <input type = "text" name="email" /> </br>
+ <input type = "submit" value="Log in" /> </br>
+ </form>
+ </body>
+ </html>
